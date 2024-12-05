78d48df0cf260a02f65f40ef0e65a968f1492197
Eduardo B. <146993235+kkewink@users.noreply.github.com>
2024-12-05 16:38:56 -0300

    ...

--- src/controllers/ClientController.js
+++ src/controllers/ClientController.js
@@ -1,4 +1,5 @@
 const clientService = require("../services/clientService");
+
 const clientController = {
     create: async(req,res) =>{
         try {
@@ -20,7 +21,51 @@ const clientController = {
                 msg:'Erro ao tentar criar o Client'
             });
         }
-    }
+    },
+    update: async(req,res) =>{
+        try{
+            const {id} = req.params;
+            
+            const data = {
+                nome:req.body.nome,
+                email: req.body.email
+            }
+
+            const client = await clientService.update(id,data);
+
+            if(!client){
+                return res.status(400).json({
+                    msg:'Cliente não encontrado'
+                });
+            }
+
+            return res.status(200).json({
+                msg:"Cliente atualizado com sucesso",
+            })
+        } catch(error){
+            return res.status(500).json({
+                msg:'Erro ao atualizaro o Client'
+            });
+        }
+    },
+    getAll: async(req,res) =>{
+        try {
+            const clients = await clientService.getAll();
+
+            return res.status(200).json({
+                msg:'Todos os clientes',
+                clients
+            });
+        } catch (error) {
+            return res.status(500).json({
+                msg: 'Ocorreu um erro no Servidor'
+            });
+        }
+    },
+    getOne: async(req,res) =>{
+
+    },
+    
 };
 
 module.exports = clientController;
--- src/services/clientService.js
+++ src/services/clientService.js
@@ -1,7 +1,8 @@
+const { getAll, getOne } = require('../controllers/ClientController');
 const Client = require('../models/client');
 
 const clientService = {
-    create: async(data) =>{
+    create: async (data) => {
         try {
             console.log(data)
             const client = await Client.create(data);
@@ -10,6 +11,53 @@ const clientService = {
         } catch (error) {
             console.error(error);
             throw new Error('Ocorreu um erro ao Criar Client');
+        }
+    },
+    update: async (id, data) => {
+        try {
+            const client = await Client.findByPk(id);
+            if (!client) {
+                return null;
+            }
+
+            return await client.update(data);
+
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao Atualizar Client');
+        }
+    },
+    getById: async (id) => {
+        try {
+            const client = await Client.findByPk(id);
+
+            if (!client) {
+                return null;
+            }
+            return client;
+        } catch (error) {
+            console.error(error);
+            throw new Error('Ocorre um erro ao buscar unico Client');
+        }
+    },
+    getAll: async () => {
+        try {
+            const client = await client.findAll();
+            return clients
+        } catch (error) {
+            throw new Error('Ocorreu um erro ao buscar todos Clientes');
+        }
+    },
+    delete: async (id) => {
+        try {
+            const client = await Client.findByPk(id);
+            if (!client) {
+                return null;
+            }
+
+            return await client.destroy();
+        } catch (error) {
+            console.error(error);
+            throw new Error('Ocorreu um erro ao deletar o Client');
         }
     }
 }
